3fa84b0de0bb699ee7a2ab52483db6ec8529e381
nailcankara <53586899+nailcankara@users.noreply.github.com>
2021-10-08 22:09:39 +0300

    Add files via upload

---
+++ pricecalculator.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.subheader("NFT Worth Calculator for Non-Fungible Anime Girls")
+st.write("---")
+nfag = ["Anime Girl #"+str(i).zfill(4) for i in range(10000)]
+
+selected = st.selectbox("Choose Your Non-Fungible Anime Girl", nfag)
+
+selectedNo = int(selected.split("#")[1])+1
+
+formula = selectedNo**(0.5) / 100
+
+st.write("---")
+st.write("Minimum price of choosed NFT is","**"+str(formula)+"**" , "**ETH**")
