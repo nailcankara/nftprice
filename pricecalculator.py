c56194cc6f9a17c328bc899c85b254a134beb549
nailcankara <53586899+nailcankara@users.noreply.github.com>
2021-10-11 22:53:56 +0300

    Update pricecalculator.py

--- pricecalculator.py
+++ pricecalculator.py
@@ -4,17 +4,11 @@ st.subheader("NFT Worth Calculator for Non-Fungible Anime Girls")
 st.write("---")
 nfag = ["Anime Girl #"+str(i).zfill(4) for i in range(10000)]
 
-fp = st.number_input('Enter the floor price ETH',value=0.001,min_value=0.001,step=0.001,format="%f")
+fp = st.number_input('Enter the floor price ETH',value=0.001,min_value=0.001,step=0.001,max_value=0.999,format="%f")
 selected = st.selectbox("Choose Your Non-Fungible Anime Girl", nfag)
 
 selectedNo = int(selected.split("#")[1])+1
 
-
-if fp >= 0.99:
-  fp = 0.99 + fp/1001
-
-if fp >= 0.999:
-  fp = 0.999
   
 coef = 100-fp*100
 
