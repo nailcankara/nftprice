4bb4a93f98f6e97e0bb440d7e1bd677b2f4fc31d
nailcankara <53586899+nailcankara@users.noreply.github.com>
2021-10-13 20:53:10 +0300

    Update pricecalculator.py

--- pricecalculator.py
+++ pricecalculator.py
@@ -26,5 +26,8 @@ st.write("---")
 fig, ax = plt.figure(figsize=(15,5))
 
 allP = pd.DataFrame([(i**0.5)/(100-fp*100) for i in range(1,10001)],columns=["Prices"])
-sns.lineplot(x=allP.index,y=allP.Prices.values)
-st.pyplot(fig)
+#sns.lineplot(x=allP.index,y=allP.Prices.values)
+#st.pyplot(fig)
+
+plt.plot(range(10000), [(i**0.5)/(100-fp*100) for i in range(1,10001)])
+plt.show()
